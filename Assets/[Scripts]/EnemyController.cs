c1076234a5a8e8edbad2c178ae1ba8092a748acd
Pritpal <pritpalsinghg679@gmail.com>
2022-10-12 03:11:08 -0400

    added rough camera mvment, updated ai script, added some obstacles

--- Assets/[Scripts]/EnemyController.cs
+++ Assets/[Scripts]/EnemyController.cs
@@ -6,18 +6,33 @@ public class EnemyController : MonoBehaviour
 {
     // Start is called before the first frame update
     public Transform _player;
+    [Range(800,1200)]
+    public float rSpeed = 800f;
+
+    public float _minDistance;
+    public float chaseSpeed = 0.5f;
+    Rigidbody rb;
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody>(); 
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        Vector3 direction = _player.position - transform.position;
+         Vector3 direction = transform.position - _player.position;
+        var vel = Vector2.Distance(transform.position,_player.position);
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        transform.Translate(direction*Time.deltaTime);
-       // Quaternion angleAxis = Quaternion.AngleAxis(angle, Vector3.forward);
-       // transform.eulerAngles = Vector3.forward * angle - 90;
+        
+        direction.Normalize();
+        if(vel < _minDistance)
+            transform.Translate(direction*Time.deltaTime*chaseSpeed);
+       
+        Quaternion rotation = Quaternion.LookRotation(Vector3.forward, direction);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation,rotation, Time.deltaTime  * rSpeed);
+      
     }
+
+   
+
 }
--- Assets/[Scripts]/PlayerController.cs
+++ Assets/[Scripts]/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     public float _speed;
     public float _force;
     Rigidbody2D _rigidibody;
-
+   
 
 
     // Start is called before the first frame update
@@ -24,13 +24,32 @@ public class PlayerController : MonoBehaviour
 
     public void Movement()
     {
-        float x = Input.GetAxisRaw("Horizontal") ;
-        float y = Input.GetAxisRaw("Vertical") ;
-       
-        if (x != 0.0f)
-            transform.position += new Vector3((x>0) ? 1.0f:-1.0f, 0.0f, 0.0f) * Time.deltaTime*  _speed;
-       if (y != 0.0f)
-            transform.position += new Vector3 (0.0f, (y > 0) ? 1.0f : -1.0f, 0.0f) * Time.deltaTime * _speed;
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
 
+        if (x != 0.0f)
+        {
+            transform.position += new Vector3(xM(x), 0.0f, 0.0f) * Time.deltaTime * _speed;
+          
+        }
+           
+        if (y != 0.0f)
+            transform.position += new Vector3 (0.0f, yM(y), 0.0f) * Time.deltaTime * _speed;
+
+    }
+    
+      public float xM(float x)
+    {
+        x = Input.GetAxisRaw("Horizontal");
+        if (x > 0.0f || x< 0.0f)
+          transform.localScale = new Vector3(1.5f, 0.3f, 0.0f);
+        return x;
+    }
+    public float yM(float y)
+    {
+        y = Input.GetAxisRaw("Vertical");
+        if (y > 0.0f || y < 0.0f)
+            transform.localScale = new Vector3(0.5f, 1f, 0.0f);
+        return y;
     }
 }
